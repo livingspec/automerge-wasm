27130dd30c33d63e1cf7d953d7a6f10e2be03c62
Nemanja Tosic <nemanja@livingspec.com>
2021-03-14 18:00:48 +0100

    Prepare for publish

--- dist/webpack/index.js
+++
@@ -1,6 +0,0 @@
-// @ts-ignore
-import { setDefaultBackend } from "automerge";
-// @ts-ignore
-import * as wasmBackend from "@livingspec/automerge-backend-wasm";
-setDefaultBackend(wasmBackend);
-export * from "automerge";
